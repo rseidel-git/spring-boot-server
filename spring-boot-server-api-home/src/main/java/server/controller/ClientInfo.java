ec7203dea5c61ebf3f91b7dac93ec0e0a76a6cdd
rafi seidel <rafi.seidel@gmail.com>
2018-05-08 15:53:51 +0300

    add logic

--- spring-boot-server-api-home/src/main/java/server/controller/ClientInfo.java
+++ spring-boot-server-api-home/src/main/java/server/controller/ClientInfo.java
@@ -1,19 +1,40 @@
 package server.controller;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public class ClientInfo {
 
     private Instant startTimeFrame;
     private int numOfRequestsInCurrentTimeFrame = 0;
 
+    public ClientInfo() {
+        this.startTimeFrame = startTimeFrame;
+    }
+
     public Instant getStartTimeFrame() {
         return startTimeFrame;
     }
 
-    public void setStartTimeFrame(Instant startTimeFrame) {
-        this.startTimeFrame = startTimeFrame;
+
+    public boolean request(Instant requestTime) {
+        final Instant now = Instant.now();
+        if (numOfRequestsInCurrentTimeFrame == 0) {
+            numOfRequestsInCurrentTimeFrame++;
+            startTimeFrame = now;
+            return true;
+        }
+        else {
+            if (numOfRequestsInCurrentTimeFrame == 5 || requestTime.minus(5, ChronoUnit.SECONDS).isAfter(startTimeFrame)) {
+                numOfRequestsInCurrentTimeFrame = 0;
+                return false;
+            }
+            else {
+                numOfRequestsInCurrentTimeFrame++;
+                return true;
+            }
+        }
+
     }
 
-    
 }
--- spring-boot-server-api-home/src/main/java/server/controller/HomeController.java
+++ spring-boot-server-api-home/src/main/java/server/controller/HomeController.java
@@ -7,36 +7,57 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import sun.misc.Contended;
 
-import java.util.HashMap;
+import javax.annotation.PreDestroy;
+import java.time.Instant;
 import java.util.Map;
+import java.util.concurrent.*;
 
 @RestController
 @RequestMapping("")
 public class HomeController {
 
+    private ExecutorService executorService = Executors.newFixedThreadPool(5);
     private static final String PARAM_NAME_CLIENT_ID = "clientId";
-    private Map<String, ClientInfo> clients = new HashMap<>();
+
+    @Contended
+    private Map<Long, ClientInfo> clients = new ConcurrentHashMap<>();
 
     @GetMapping("/")
-    public ResponseEntity<Void> home(@RequestParam(PARAM_NAME_CLIENT_ID) int clientId) {
+    public ResponseEntity<Void> home(@RequestParam(PARAM_NAME_CLIENT_ID) long clientId) {
 
+        final Future<Boolean> future = executorService.submit(new ServerTask(clientId));
+        try {
+            if (!future.get(10, TimeUnit.SECONDS)) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    private class ServerTask implements Runnable {
+    @PreDestroy
+    private void destroy() throws InterruptedException {
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+        executorService.shutdownNow();
+    }
+
+    private class ServerTask implements Callable<Boolean> {
 
         private final long clientId;
 
         private ServerTask(long clientId) {
             this.clientId = clientId;
         }
-
         @Override
-        public void run() {
+        public Boolean call() {
+            final Instant now = Instant.now();
+            clients.putIfAbsent(clientId, new ClientInfo());
             final ClientInfo clientInfo = clients.get(clientId);
-            clientInfo.getStartTimeFrame();
-            clientInfo.getStartTimeFrame();
+            return clientInfo.request(now);
         }
     }
 }
